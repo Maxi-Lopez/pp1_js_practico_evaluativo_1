a00958e9adb4233028d65feaebf0d4ff1da7ce09
Maxi_Lopez <m.lopez@itecriocuarto.org.ar>
2025-06-26 08:38:31 -0300

    se modifico la logica de filtros

--- js/libros.js
+++ js/libros.js
@@ -1,63 +1,48 @@
 let libros = JSON.parse(localStorage.getItem('libros')) || []
 let editando = false;
 let indiceEditar = null;
-<<<<<<< HEAD
 let orden = true;
-=======
->>>>>>> 4f354bfa4e3d11b764e1d7890f556909ee955a5b
+
+const capitalizarPrimerLetra = (str) => {
+    return str
+        .toLowerCase() 
+        .replace(/\b\w/g, (letra) => letra.toUpperCase()); 
+}
 
 const agregarLibro = () => {
-    const titulo = document.getElementById('titulo').value.trim()
-    const autor = document.getElementById('autor').value.trim()
+    const titulo = capitalizarPrimerLetra(document.getElementById('titulo').value.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
+    const autor = capitalizarPrimerLetra(document.getElementById('autor').value.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
     const anio = Number(document.getElementById('anio').value)
     const genero = document.getElementById('genero').value.trim()
 
-    const tituloValido = titulo !== ''
-    const autorValido = autor !== ''
-    const generoValido = genero !== ''
     const anioActual = new Date().getFullYear()
 
-    if (tituloValido && autorValido && generoValido) {
-        if (editando) {
-            if (isNaN(anio) || anio < 1900 || anio > anioActual) {
-                alert('El año debe estar entre 1900 y ' + anioActual)
-                return
-            }
-<<<<<<< HEAD
-            libros[indiceEditar] = { titulo, autor, anio, genero, leido }
-=======
-            libros[indiceEditar] = { titulo, autor, anio, genero }
->>>>>>> 4f354bfa4e3d11b764e1d7890f556909ee955a5b
-            editando = false
-            indiceEditar = null
-            document.querySelector('button[type="submit"]').innerText = 'Agregar'
-        } else {
-            const anioValido = !isNaN(anio) && anio >= 1900 && anio <= anioActual
-            if (!anioValido) {
-                alert('El año debe estar entre 1900 y ' + anioActual)
-                return
-            }
-
-            const existe = libros.some(libro => libro.titulo.toLowerCase() === titulo.toLowerCase() && libro.autor.toLowerCase() === autor.toLowerCase())
-            if (existe) {
-                alert('El libro ya existe')
-                return
-            }
-
-<<<<<<< HEAD
-            libros.push({ titulo, autor, anio, genero, leido: false })
-=======
-            libros.push({ titulo, autor, anio, genero })
->>>>>>> 4f354bfa4e3d11b764e1d7890f556909ee955a5b
+    if (anio < 1900 || anio > anioActual) {
+        alert('El año debe estar entre 1900 y ' + anioActual)
+        return
+    }
+    if (editando) {
+        libros[indiceEditar] = { titulo, autor, anio, genero, leido: libros[indiceEditar].leido }
+        editando = false
+        indiceEditar = null
+        document.querySelector('button[type="submit"]').innerText = 'Agregar'
+    } else {
+        const existe = libros.some(libro =>
+            libro.titulo.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") === titulo.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") &&
+            libro.autor.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") === autor.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        )
+        if (existe) {
+            alert('El libro ya existe')
+            return
         }
 
-        localStorage.setItem('libros', JSON.stringify(libros))
-        renderizarLibros()
-        mostrarResumen(libros)
-        actualizarSelectGenero()
-    } else {
-        alert('Todos los campos deben estar completos')
+        libros.push({ titulo, autor, anio, genero, leido: false })
     }
+
+    localStorage.setItem('libros', JSON.stringify(libros))
+    renderizarLibros()
+    mostrarResumen(libros)
+    actualizarSelectGenero()
 
     document.getElementById('titulo').value = ''
     document.getElementById('autor').value = ''
@@ -65,8 +50,7 @@ const agregarLibro = () => {
     document.getElementById('genero').value = ''
 }
 
-
-const filtrarLibros = () => {
+const buscarLibros = () => {
     const texto = document.getElementById('busqueda').value.toLowerCase()
     const librosFiltrados = libros.filter(libro => libro.titulo.toLowerCase().includes(texto))
     renderizarLibros(librosFiltrados)
@@ -90,7 +74,6 @@ const renderizarLibros = (lista = libros) => {
                 <button onclick="eliminarLibro(${indexReal})">Eliminar</button>
                 <button onclick="editarLibro(${indexReal})">Editar</button>
             </td>
-<<<<<<< HEAD
             <td>
             <label>
                 <input type="checkbox" class="leido-checkbox" data-index="${indexReal}" ${libro.leido ? 'checked' : ''}>
@@ -106,10 +89,6 @@ const renderizarLibros = (lista = libros) => {
             localStorage.setItem('libros', JSON.stringify(libros))
             mostrarResumen(libros)
     })
-=======
-        `
-        tabla.appendChild(fila)
->>>>>>> 4f354bfa4e3d11b764e1d7890f556909ee955a5b
     })
 }
 
@@ -138,7 +117,6 @@ const mostrarResumen = (libros = libros) => {
     const posterioresA2010 = libros.filter(libro => libro.anio > 2010).length
     const libroNuevo = libros.reduce((nuevo, libro) => (libro.anio > nuevo.anio ? libro : nuevo), libros[0])
     const libroViejo = libros.reduce((viejo, libro) => (libro.anio < viejo.anio ? libro : viejo), libros[0])
-<<<<<<< HEAD
     const leidos = libros.filter(libro => libro.leido).length
     const noLeidos = libros.filter(libro => !libro.leido).length
 
@@ -150,51 +128,35 @@ const mostrarResumen = (libros = libros) => {
         <p><strong>Libro más viejo: </strong>${libroViejo.titulo} de ${libroViejo.autor}, ${libroViejo.anio}</p>
         <p><strong>Cantidad de libros leídos: </strong>${leidos}</p>
         <p><strong>Cantidad de libros no leídos: </strong>${noLeidos}</p>
-=======
-
-    resumen.innerHTML = `
-        <p>Total de libros: ${total}</p>
-        <p>Promedio de año de edición: ${promedio}</p>
-        <p>Libros posteriores a 2010: ${posterioresA2010}</p>
-        <p>Libro más nuevo: ${libroNuevo.titulo} de ${libroNuevo.autor}, ${libroNuevo.anio}</p>
-        <p>Libro más viejo: ${libroViejo.titulo} de ${libroViejo.autor}, ${libroViejo.anio}</p>
->>>>>>> 4f354bfa4e3d11b764e1d7890f556909ee955a5b
     `
 }
 
-const filtrarPorGenero = () => {
+
+const filtrarLibros = () => {
     const genero = document.getElementById('filtroGenero').value
-    if (genero === 'todos') {
-        renderizarLibros()
-        mostrarResumen(libros)
-    } else {
-        const librosFiltrados = libros.filter(libro => libro.genero === genero)
-        renderizarLibros(librosFiltrados)
-        mostrarResumen(librosFiltrados)
-    }
-}
-
-
-<<<<<<< HEAD
-const filtrarPorLeido = () => {
     const leido = document.getElementById('filtroLeido').value
-    if (leido === 'todos') {
-        renderizarLibros()
-        mostrarResumen(libros)
-    } 
-    if (leido === 'leidos') {
-        const librosFiltrados = libros.filter(libro => libro.leido === true)
-        renderizarLibros(librosFiltrados)
-        mostrarResumen(librosFiltrados)
 
-    }
-    if (leido === 'noLeidos') {
-        const librosFiltrados = libros.filter(libro => libro.leido === false)
-        renderizarLibros(librosFiltrados)
-        mostrarResumen(librosFiltrados)
+    let librosFiltrados = [...libros]; 
 
+    if (genero !== 'todos') {
+        librosFiltrados = librosFiltrados.filter(libro => libro.genero === genero)
     }
+
+    if (leido !== 'todos') {
+        let esLeido
+        if (leido === 'leidos') {
+            esLeido = true
+        } else {
+            esLeido = false
+        }
+        librosFiltrados = librosFiltrados.filter(libro => libro.leido === esLeido);
+    }
+
+    renderizarLibros(librosFiltrados);
+    mostrarResumen(librosFiltrados);
 }
+
+
 
 const ordenarLibro = () => {
     if (orden) {
@@ -211,8 +173,6 @@ const ordenarLibro = () => {
 
 
 
-=======
->>>>>>> 4f354bfa4e3d11b764e1d7890f556909ee955a5b
 const actualizarSelectGenero = () => {
     const select = document.getElementById('filtroGenero')
     const generosUnicos = [...new Set(libros.map(libro => libro.genero))]
@@ -238,15 +198,3 @@ document.addEventListener('DOMContentLoaded', () => {
     mostrarResumen(libros)
     actualizarSelectGenero()
 })
-<<<<<<< HEAD
-=======
-
-
-
-/* Falta agregar funcion de ordenar por año de publicacion
-Y tambien hacer el bonus
-*** chebox (leido)
-*** Mostrar en el resumen cuántos libros fueron leídos / no leídos
-*** Filtro adicional para mostrar solo leídos / no leídos
-*/
->>>>>>> 4f354bfa4e3d11b764e1d7890f556909ee955a5b
